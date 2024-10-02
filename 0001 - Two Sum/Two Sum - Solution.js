2a3c6e7c0207555ff8c422c933c79dc937cdcedd
Moatassem Ahmed <102925713+Moatassem5@users.noreply.github.com>
2024-10-02 22:35:26 +0300

    Create Two Sum - Solution.js

---
+++ 0001 - Two Sum/Two Sum - Solution.js
@@ -0,0 +1,10 @@
+
+const twoSum = function (nums, target) {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let x = i + 1; x < nums.length; x++) {
+      if (nums[x] + nums[i] === target) {
+        return ([i, x]);;
+      }
+    }
+  }
+};
